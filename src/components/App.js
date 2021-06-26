b3710875a657cab4565a16b9eced81dc9b1b4708
ychalodiya <ychalodiya@gmail.com>
2021-06-26 00:44:37 -0400

    Initializing QuickSale Project

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+export default class App extends Component {
+    render() {
+        return (
+            <View><Text>QuickSale</Text></View>
+        );
+    }
+}
